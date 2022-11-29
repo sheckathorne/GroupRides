298c4265ca637b3fdbca18a9da1686883d73d9e7
Sean Heckathorne <heckathorne.s@gmail.com>
2022-11-28 20:08:02 -0600

    final layout of event add form

--- grouprides/groupridesapp/forms.py
+++ grouprides/groupridesapp/forms.py
@@ -56,6 +56,14 @@ class CreateClubForm(forms.ModelForm):
         self.fields['private'].label = 'Private (membership managed by Admin)'
 
 
+def text_input(field_name, width=4):
+    return Div(Field(field_name), css_class=f"col-md-{width}", )
+
+
+def dropdown(field_name, height=38, width=4):
+    return Div(Field(field_name, css_class="w-100", style=f"height: {height}px;"), css_class=f"col-md-{width}", )
+
+
 class CreateEventForm(forms.ModelForm):
     class Meta:
         model = Event
@@ -76,6 +84,7 @@ class CreateEventForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super(CreateEventForm, self).__init__(*args, **kwargs)
+        self.fields['frequency'].label = 'Recurrence'
         self.fields['route'].queryset = Route.objects.filter(created_by=self.user)
         self.fields['club'].queryset = (
             Club.objects.filter(
@@ -86,46 +95,24 @@ class CreateEventForm(forms.ModelForm):
         self.helper = FormHelper(self)
         self.helper.layout = Layout(
             Fieldset('Ride Info',
-                     Div(
-                         Div(Field('name'), css_class='col-md-4', ),
-                         css_class='row',
-                     ),
-                     Div(
-                         Div(Field('route', css_class="w-100", style="height: 38px;"), css_class='col-md-4', ),
-                         css_class='row',
-                     ),
-                     Div(
-                         Div(Field('privacy', css_class="w-100", style="height: 38px;"), css_class='col-md-4', ),
-                         css_class='row',
-                     ),
-                     Div(
-                         Div(Field('club', css_class="w-100", style="height: 38px;", type="date"), css_class='col-md-4', ),
-                         css_class='row',
-                     ), css_class='mt-4'),
-            Fieldset('Pace',
-                     Div(
-                         Div(
-                             Field('group_classification', css_class="w-100", style="height: 38px;")
-                             , css_class='col-md-4'
-                         ),
-                         Div('lower_pace_range', css_class='col-md-4', ),
-                         Div('upper_pace_range', css_class='col-md-4', ),
-                         css_class='row',
-                     ), css_class='mt-4'),
-            Fieldset('Date / Time / Recurring',
-                     Div(
-                         Div('start_date', css_class='col-md-4', ),
-                         Div('end_date', css_class='col-md-4', ),
-                         css_class='row',
-                     ),
-                     Div(
-                         Div('ride_time', css_class='col-md-4', ),
-                         Div(
-                             Field('time_zone', css_class="w-100", style="height: 38px;")
-                             , css_class='col-md-4'
-                         ),
-                         css_class='row',
-                     ),css_class='mt-4')
-            ,)
-
-        start_date = forms.DateField(widget=SelectDateWidget(empty_label="Nothing"))
+                     Div(text_input("name"), dropdown("club"), css_class='row mb-3'),
+                     Div(dropdown("route"), dropdown("privacy"), css_class='row mb-3', ),
+                     Div(text_input("max_riders"), css_class='row mb-3', ),
+                     css_class='mt-4'),
+            Fieldset('Pace', Div(
+                dropdown("group_classification"),
+                text_input("lower_pace_range"),
+                text_input("upper_pace_range"),
+                css_class='row mb-3', ),
+                     css_class='mt-4'),
+            Fieldset('Date / Time / Recurring', Div(
+                text_input("start_date"),
+                dropdown("time_zone"),
+                css_class='row mb-3', ), Div(
+                text_input("end_date"),
+                text_input("ride_time"),
+                css_class='row mb-3', ), Div(
+                dropdown("frequency"),
+                css_class='row mb-3', ),
+                     css_class='mt-4'),
+        )
