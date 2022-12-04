71305a3d167d1d91fcbf276720a69d1e82d0710b
Sean Heckathorne <heckathorne.s@gmail.com>
2022-12-04 08:46:41 -0600

    some bug fixes

--- grouprides/static/groupridesapp/js/create_event.js
+++ grouprides/static/groupridesapp/js/create_event.js
@@ -1,4 +1,24 @@
+function showHelpText(el, url) {
+    el.show();
+    el.text(url);
+    el.attr("href")
+
+}
+
 $(document).ready(function() {
+    var option = $('#event_create_route').find(':selected');
+    var url = option.data('url');
+    var help_text = $('#route_url_id');
+
+    if ( url && url.length > 0 ) {
+        help_text.show();
+        help_text.text(url);
+        help_text.attr("href", url);
+    } else {
+        help_text.hide();
+    }
+
+
     if ($("#event_create_privacy").val() === "5") {
         conditional_fields.show();
     } else {
@@ -17,7 +37,16 @@ $("#event_create_privacy").change(function() {
     }
 });
 
-$("#event_create_route").change(function() {
-    var url = `$(this).prop("data-url")
-    console.log(url)
+$("#event_create_route").on('change', function() {
+    var option = $(this).find(':selected');
+    var url = option.data('url');
+    var help_text = $('#route_url_id');
+
+    if ( url && url.length > 0 ) {
+        help_text.show();
+        help_text.attr("href", url);
+        help_text.text(url);
+    } else {
+        help_text.hide();
+    }
 })
